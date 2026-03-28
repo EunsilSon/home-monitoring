a18554433c5d3545785d3ed520f361aff9225be8
EunsilSon <eunsil1023@gmail.com>
2026-03-28 20:29:31 +0900

    chore: setup spring boot configuration

---
+++ home-monitoring-server/src/main/java/com/eunsilson/homemonitoring/HomemonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.eunsilson.homemonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomemonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomemonitoringApplication.class, args);
+	}
+
+}
---
+++ home-monitoring-server/src/test/java/com/eunsilson/homemonitoring/HomemonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunsilson.homemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
