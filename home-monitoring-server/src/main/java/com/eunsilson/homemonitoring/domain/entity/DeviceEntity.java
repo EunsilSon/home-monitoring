88bd7244eba9cb6536188b5b3256a757dbb2ecd5
EunsilSon <eunsil1023@gmail.com>
2026-03-29 21:41:40 +0900

    fix: update annotations and fields

--- home-monitoring-server/src/main/java/com/eunsilson/homemonitoring/domain/entity/DeviceEntity.java
+++ home-monitoring-server/src/main/java/com/eunsilson/homemonitoring/domain/entity/DeviceEntity.java
@@ -1,19 +1,19 @@
 package com.eunsilson.homemonitoring.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 import java.time.Instant;
 import java.util.UUID;
 
-@Data
+@Entity
 @Table(name = "device")
 public class DeviceEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    public UUID id;
-    public String name;
-    public String description;
-    public Instant createdAt;
-    public Instant updatedAt;
+    private UUID id;
+    private String name;
+    private String description;
+    private Instant createdAt;
+    private Instant updatedAt;
+
 }
--- home-monitoring-server/src/main/java/com/eunsilson/homemonitoring/domain/entity/SensorDataEntity.java
+++ home-monitoring-server/src/main/java/com/eunsilson/homemonitoring/domain/entity/SensorDataEntity.java
@@ -2,23 +2,26 @@ package com.eunsilson.homemonitoring.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.util.UUID;
 
+@NoArgsConstructor
 @Data
+@Entity
 @Table(name = "sensor_data")
 public class SensorDataEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
-    public UUID deviceId;
-    public Float temperature;
-    public Float humidity;
-    public Float heatIndex;
-    public Instant recordedAt;
+    private Long id;
+    private UUID deviceId;
+    private float temperature;
+    private float humidity;
+    private float heatIndex;
+    private Instant recordedAt;
 
-    public SensorDataEntity(UUID deviceId, Float temperature, Float humidity, Float heatIndex, Instant recordedAt) {
+    public SensorDataEntity(UUID deviceId, float temperature, float humidity, float heatIndex, Instant recordedAt) {
         this.deviceId = deviceId;
         this.temperature = temperature;
         this.humidity = humidity;
