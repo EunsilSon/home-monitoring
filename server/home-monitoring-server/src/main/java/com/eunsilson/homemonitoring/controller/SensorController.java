b27e23a8caac817d24ff51bb9834381f206e4856
EunsilSon <eunsil1023@gmail.com>
2026-04-02 00:01:14 +0900

    chore: rename folder

---
+++ server/home-monitoring-server/src/main/java/com/eunsilson/homemonitoring/controller/SensorController.java
@@ -0,0 +1,30 @@
+package com.eunsilson.homemonitoring.controller;
+
+import com.eunsilson.homemonitoring.domain.dto.SensorDataRequest;
+import com.eunsilson.homemonitoring.domain.entity.SensorLatestEntity;
+import com.eunsilson.homemonitoring.service.SensorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sensor")
+@RequiredArgsConstructor
+public class SensorController {
+    private final SensorService sensorService;
+
+    @PostMapping("/bulk")
+    public ResponseEntity<Void> save(@RequestBody List<SensorDataRequest> requests) {
+        if (sensorService.saveSensorDataAndLatestUpdate(requests)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.internalServerError().build();
+    }
+
+    @GetMapping("/latest")
+    public ResponseEntity<SensorLatestEntity> getLatest() {
+        return ResponseEntity.ok().body(sensorService.getLatest());
+    }
+}
