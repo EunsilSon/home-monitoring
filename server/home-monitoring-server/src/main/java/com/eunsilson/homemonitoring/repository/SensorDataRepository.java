1c738e218c49889e74977ddb27e6b0ae8818f02e
EunsilSon <46162801+EunsilSon@users.noreply.github.com>
2026-06-21 15:48:13 +0900

    fix: entity pk type

--- server/home-monitoring-server/src/main/java/com/eunsilson/homemonitoring/repository/SensorDataRepository.java
+++ server/home-monitoring-server/src/main/java/com/eunsilson/homemonitoring/repository/SensorDataRepository.java
@@ -3,7 +3,5 @@ package com.eunsilson.homemonitoring.repository;
 import com.eunsilson.homemonitoring.domain.entity.SensorDataEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface SensorDataRepository extends JpaRepository<SensorDataEntity, UUID> {
+public interface SensorDataRepository extends JpaRepository<SensorDataEntity, Long> {
 }
